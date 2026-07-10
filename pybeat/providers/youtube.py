0b618235f0d22e01b129caee9505da343adcfc35
coderkuze-code <coderkuze@gmail.com>
2026-07-10 23:43:10 +0530

    Update youtube.py

--- pybeat/providers/youtube.py
+++ pybeat/providers/youtube.py
@@ -3,5 +3,9 @@ from .base import BaseProvider
 
 class YouTubeProvider(BaseProvider):
 
+    def __init__(self, http):
+        self.http = http
+
     async def search(self, query: str):
+
         return []
