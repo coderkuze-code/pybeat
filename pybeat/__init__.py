5c32f92eff319d749e3097e7bb0e613bcda530f2
coderkuze-code <coderkuze@gmail.com>
2026-07-10 23:48:45 +0530

    Include version metadata in __all__
    Added version metadata to the module's exports.

--- pybeat/__init__.py
+++ pybeat/__init__.py
@@ -1,5 +1,17 @@
 from .client import Client
+from .version import (
+    __author__,
+    __description__,
+    __license__,
+    __title__,
+    __version__,
+)
 
-__version__ = "0.0.1"
-
-__all__ = ["Client"]
+__all__ = [
+    "Client",
+    "__author__",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__version__",
+]
