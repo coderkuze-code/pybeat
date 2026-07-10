bc0887cc85a14ae63f20ff991d64d874c45f7c00
coderkuze-code <coderkuze@gmail.com>
2026-07-10 23:48:13 +0530

    Add version information to pybeat/version.py

---
+++ pybeat/version.py
@@ -0,0 +1,8 @@
+__title__ = "pybeat"
+__description__ = "A modern asynchronous Python media library."
+__version__ = "0.0.1"
+
+__author__ = "PyBeat Developers"
+__license__ = "MIT"
+
+VERSION = tuple(map(int, __version__.split(".")))
